f2f4f6ce6be0308371a762848cd8cebda465025d
sagar <sgrneaneprof@gmail.com>
2024-02-05 18:26:07 +0545

    database switched

--- membership/settings.py
+++ membership/settings.py
@@ -80,21 +80,6 @@ WSGI_APPLICATION = "membership.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'new_membership', #lims_new
-        'USER': 'root',
-        'PASSWORD':'',
-        'HOST':'localhost',
-        'PORT': '3308',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            'charset': 'utf8mb4',
-        }
-    }
-}
-
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
@@ -102,7 +87,7 @@ DATABASES = {
 #         'USER': 'root',
 #         'PASSWORD':'',
 #         'HOST':'localhost',
-#         'PORT': '3308',#3308
+#         'PORT': '3308',
 #         'OPTIONS': {
 #             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
 #             'charset': 'utf8mb4',
@@ -110,16 +95,22 @@ DATABASES = {
 #     }
 # }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'kantggjb_membershipdb', #lims_new
+        'USER': 'kantggjb_membershipuser',
+        'PASSWORD':'b2jXog&4--R?',
+        'HOST':'localhost',
+        'PORT': '3306',#3308
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        }
+    }
+}
 
 
-# Password validation
-# https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
